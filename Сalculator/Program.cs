910fa150b79b647c7875073ae7838976d473f30f
PickOleni <alina24052001@gmail.com>
2024-08-17 09:01:29 +1000

    v2

--- Сalculator/Program.cs
+++ Сalculator/Program.cs
@@ -1,63 +1,82 @@
-﻿namespace Сalculator
+﻿using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Сalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое значение:");
-            string x1 = Console.ReadLine();
+            bool isUserWannaContinue = true;
+            while (isUserWannaContinue)
+            {
+                Console.WriteLine("Введите первое значение:");
 
-            if (!int.TryParse(x1, out int x))
-            {
-                Console.WriteLine("Значение набранно некорректно.");
-                return;
-            }
+                int number1 = Input("Значение набранно некорректно.");
+                Console.WriteLine("Введите второе значение:");
+                int number2 = Input("И что теперь, будем складывать буквы?");
 
-            Console.WriteLine("Введите второе значение:");
-            string y1 = Console.ReadLine();
-            if (!int.TryParse(y1, out int y))
-            {
-                Console.WriteLine("И что теперь, будем складывать буквы?");
-                return;
+                Console.WriteLine("Выберите действие: +, -, *, /.");
+                string mathAction = Console.ReadLine();
+
+                if (number2 == 0 && mathAction == "/")
+                {
+                    Console.WriteLine("Шарик, ты балбес! На ноль делить нельзя.");
+                    continue;
+                }
+
+                Count(number1, number2, mathAction);
+
+                string hotkeyForExit = "н";
+                Console.WriteLine($"Если хотите закончить - введите {hotkeyForExit}");
+                string programmRepeat = Console.ReadLine();
+
+                if (programmRepeat == hotkeyForExit)
+                {
+                    isUserWannaContinue = false;
+                    Console.WriteLine("До скорых встреч!");
+                    return;
+                }
             }
-      
-            Console.WriteLine("Выберите действие: +, -, *, /.");
-            string mathAction = Console.ReadLine();
-            if (mathAction != "+" && mathAction != "-" && mathAction != "*" && mathAction != "/")
-            {
-                Console.WriteLine("Я пока не умею так считать.");
-                return;
-            }
-           
-            if (y == 0 && mathAction == "/") 
-            {
-                Console.WriteLine("Шарик, ты балбес! На ноль делить нельзя.");
-                return;
-            }
-             
-            Count(x, y, mathAction);
         }
 
-        static void Count(int x, int y, string mathAction)
+        static int Input(string errorText)
         {
+            string inputNumber = Console.ReadLine();
+
+            if (!int.TryParse(inputNumber, out int number))
+            {
+                Console.WriteLine($"{errorText}");
+            }
+            return number;
+        }
+
+        static void Count( int number1, int number2, string mathAction)
+        {
+
+            int answer = 0;
             
             switch (mathAction)
             {
                 case ("+"):
-                    int a = x + y;
-                    Console.WriteLine($"{a}");
+                    answer = number1 + number2;
+                    Console.WriteLine($"{answer}");
                     break;
                 case ("-"):
-                    int b = x - y;
-                    Console.WriteLine($"{b}");
+                    answer = number1 - number2;
+                    Console.WriteLine($"{answer}");
                     break;
                 case ("*"):
-                    int c = x * y;
-                    Console.WriteLine($"{c}");
+                    answer = number1 * number2;
+                    Console.WriteLine($"{answer}");
                     break;
                 case ("/"):
-                    int d = x / y;
-                    Console.WriteLine($"{d}");
+                    answer = number1 / number2;
+                    Console.WriteLine($"{answer}");
+                    break;
+                default:
+                    Console.WriteLine("Я пока не умею так считать.");
                     break;
 
             }
